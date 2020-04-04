b03190118aa2c3b62edc306b2b84a83f590f5806
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-04-04 20:45:43 -0300

    fix template urls

--- src/helpers/templates.ts
+++ src/helpers/templates.ts
@@ -4,14 +4,14 @@ import tar from "tar";
 
 export function downloadAndExtractTemplate(root: string, name: string): Promise<void> {
   return promisePipe(
-    got.stream("https://codeload.github.com/patitonar/create-solidity-project/tar.gz/master"),
-    tar.extract({ cwd: root, strip: 3 }, [`create-solidity-project-master/templates/${name}`]),
+    got.stream("https://codeload.github.com/patitonar/create-solidity-contract/tar.gz/master"),
+    tar.extract({ cwd: root, strip: 3 }, [`create-solidity-contract-master/templates/${name}`]),
   );
 }
 
 export function hasTemplate(name: string): Promise<boolean> {
   return isUrlOk(
-    `https://api.github.com/repos/patitonar/create-solidity-project/contents/templates/${encodeURIComponent(
+    `https://api.github.com/repos/patitonar/create-solidity-contract/contents/templates/${encodeURIComponent(
       name,
     )}/package.json`,
   );
