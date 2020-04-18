41b9e517e62edfa139ee251fe72f566e62850414
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-04-18 17:10:41 -0300

    templates cleanup

--- templates/buidler/test/sample-test.js
+++ templates/buidler/test/sample-test.js
@@ -1,18 +1,5 @@
 const Greeter = artifacts.require("Greeter");
 
-// Traditional Truffle test
-contract("Greeter", accounts => {
-  it("Should return the new greeting once it's changed", async function() {
-    const greeter = await Greeter.new("Hello, world!");
-    assert.equal(await greeter.greet(), "Hello, world!");
-
-    await greeter.setGreeting("Hola, mundo!");
-
-    assert.equal(await greeter.greet(), "Hola, mundo!");
-  });
-});
-
-// Vanilla Mocha test. Increased compatibility with tools that integrate Mocha.
 describe("Greeter contract", function() {
   let accounts;
 
