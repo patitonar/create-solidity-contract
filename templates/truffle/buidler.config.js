2dc3910fb5a34d200276aea38ed2f05ce36a4736
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-05-10 18:41:39 -0300

    Add @openzeppelin/contracts in truffle template

--- templates/truffle/buidler.config.js
+++ templates/truffle/buidler.config.js
@@ -1,4 +1,8 @@
 usePlugin('@nomiclabs/buidler-truffle5')
 usePlugin('solidity-coverage')
 
-module.exports = {}
+module.exports = {
+  solc: {
+    version: '0.6.7',
+  },
+}
--- templates/truffle/contracts/MetaCoin.sol
+++ templates/truffle/contracts/MetaCoin.sol
@@ -1,11 +1,15 @@
 pragma solidity >=0.4.25 <0.7.0;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
 
 // This is just a simple example of a coin-like contract.
 // It is not standards compatible and cannot be expected to talk to other
 // coin/token contracts. If you want to create a standards-compliant
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 contract MetaCoin {
+    using SafeMath for uint256;
+
     mapping(address => uint256) internal balances;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -16,14 +20,14 @@ contract MetaCoin {
 
     function sendCoin(address receiver, uint256 amount) public returns (bool sufficient) {
         if (balances[msg.sender] < amount) return false;
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
+        balances[msg.sender] = balances[msg.sender].sub(amount);
+        balances[receiver] = balances[receiver].add(amount);
         emit Transfer(msg.sender, receiver, amount);
         return true;
     }
 
     function getBalanceInEth(address addr) public view returns (uint256) {
-        return getBalance(addr) * 2;
+        return getBalance(addr).mul(2);
     }
 
     function getBalance(address addr) public view returns (uint256) {
